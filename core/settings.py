97649b99f47a3d9bf6020a8235570f4c65b6a265
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2019-10-28 14:06:40 +0700

    Configure settings for static files Add bootstrap, add base and partials templates, and update gitignore.

--- core/settings.py
+++ core/settings.py
@@ -111,5 +111,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'countries/static'
+]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
