bb634958cee0b8c476d23e560cfa5d309d2b3f40
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2019-10-28 13:37:08 +0700

    List all countries on homepage

--- countries/views.py
+++ countries/views.py
@@ -1,5 +1,30 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    countries = get_list_of_countries()
+    return render(request, 'index.html', {'countries': countries})
+
+
+def get_list_of_countries():
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery("""
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX dbr: <http://dbpedia.org/resource/>
+
+        SELECT ?country ?capital
+        WHERE {
+         ?country a dbo:Country.
+         ?country dbo:capital ?capital.
+         FILTER NOT EXISTS { ?country dbo:dissolutionYear ?yearEnd }
+        }
+    """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    list_of_countries = []
+    for result in results["results"]["bindings"]:
+        list_of_countries.append(result["country"]["value"])
+
+    return list_of_countries
