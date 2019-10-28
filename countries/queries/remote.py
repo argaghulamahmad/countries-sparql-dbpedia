3c7d6f2f6ba85ad41fcb5c8c10b58f4210c118a6
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2019-10-28 15:21:53 +0700

    Implement search country feature

--- countries/queries/remote.py
+++ countries/queries/remote.py
@@ -16,10 +16,28 @@ def get_list_of_countries():
          FILTER NOT EXISTS { ?country dbo:dissolutionYear ?yearEnd }.
          FILTER (langMatches(lang(?country_name), "EN")).
          FILTER (langMatches(lang(?country_capital), "EN")).
-         FILTER (regex(?country_name, "indonesia", "i" )).
         }
         ORDER BY ASC(?country_name)
     """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()["results"]["bindings"]
+
+    list_of_countries = []
+    for result in results:
+        list_of_countries.append(result['country_name']['value'])
+
+    return list_of_countries
+
+
+def filter_list_of_countries(keyword):
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery(
+        "PREFIX dbo: <http://dbpedia.org/ontology/>PREFIX dbr: <http://dbpedia.org/resource/>SELECT distinct "
+        "?country_name ?country_capitalWHERE {?country a dbo:Country.?country rdfs:label ?country_name.?country "
+        "dbo:capital ?capital.?capital rdfs:label ?country_capital.FILTER NOT EXISTS { ?country dbo:dissolutionYear "
+        "?yearEnd }.FILTER (langMatches(lang(?country_name), \"EN\")).FILTER (langMatches(lang(?country_capital), "
+        "\"EN\")).FILTER (regex(?country_name, \"" + keyword + "\", \"i\" )).}ORDER BY ASC(?country_name) "
+    )
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()["results"]["bindings"]
 
--- countries/views.py
+++ countries/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
-from countries.queries.remote import get_list_of_countries
+from countries.queries.remote import get_list_of_countries, filter_list_of_countries
 
 
 def index(request):
     countries = get_list_of_countries()
+    return render(request, 'index.html', {'countries': countries})
+
+
+def search(request, keyword):
+    countries = filter_list_of_countries(keyword)
     return render(request, 'index.html', {'countries': countries})
