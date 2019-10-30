8384a30b8f57760e61d65a3366f6860e498547bd
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2019-10-30 09:27:34 +0700

    Displaying remote data to detail page

--- countries/queries/remote.py
+++ countries/queries/remote.py
@@ -53,7 +53,7 @@ def information_of_a_country(keyword):
         "        PREFIX dbo: <http://dbpedia.org/ontology/>\n"
         "        PREFIX dbr: <http://dbpedia.org/resource/>\n"
         "        \n"
-        "        SELECT distinct ?country_name ?country_capital ?country_abstract ?country_language\n"
+        "        SELECT distinct ?country_name ?country_capital ?country_abstract ?country_language ?country_currency\n"
         "        WHERE {\n"
         "         ?country a dbo:Country.\n"
         "         ?country rdfs:label ?country_name.\n"
@@ -62,11 +62,14 @@ def information_of_a_country(keyword):
         "         ?country dbo:abstract ?country_abstract.\n"
         "         ?country dbo:language ?language.\n"
         "         ?language rdfs:label ?country_language.\n"
+        "         ?country dbo:currency ?currency.\n"
+        "         ?currency rdfs:label ?country_currency.\n"
         "         FILTER NOT EXISTS { ?country dbo:dissolutionYear ?yearEnd }.\n"
         "         FILTER (langMatches(lang(?country_name), \"EN\")).\n"
         "         FILTER (langMatches(lang(?country_capital), \"EN\")).\n"
         "         FILTER (langMatches(lang(?country_abstract), \"EN\")).\n"
         "         FILTER (langMatches(lang(?country_language), \"EN\")).\n"
+        "         FILTER (langMatches(lang(?country_currency), \"EN\")).\n"
         "         FILTER (regex(?country_name, \"" + keyword + "\", \"i\")).\n"
                                                                "        }\n"
                                                                "        ORDER BY ASC(?country_name)\n"
@@ -74,4 +77,21 @@ def information_of_a_country(keyword):
     )
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()["results"]["bindings"]
-    return results
+
+    country_name = set()
+    country_capital = set()
+    country_abstract = set()
+    country_language = set()
+    country_currency = set()
+
+    for country in results:
+        country_name.add(country["country_name"]["value"])
+        country_capital.add(country["country_capital"]["value"])
+        country_abstract.add(country["country_abstract"]["value"])
+        country_language.add(country["country_language"]["value"])
+        country_currency.add(country["country_currency"]["value"])
+
+    country_info = [list(country_name), list(country_capital), list(country_abstract), list(country_language),
+                    list(country_currency)]
+    print(country_info)
+    return country_info
